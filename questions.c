72b25f8394a328854483b095241878426ca0c49f
Marl8n <midp515@gmail.com>
2020-04-30 23:01:05 -0600

    Este fue el programa que presente

--- questions.c
+++ questions.c
@@ -5,20 +5,20 @@
 
 void menquestion()
 {
-	char mensq[][80] = {"aa","bb","cc","dd","ee","ff","gg","hh","ii","jj"};
-	int defineq, qdirectory;
-	srand(time(NULL));
-	defineq = rand();
-	qdirectory = rand() % 9;
+	char menswq[][80] = {"\n\tTe gustan los deportes de contacto?\n\t","\n\tTe gustaria ser como cr7 o messi?\n\t","\n\tprefieres ver un partido de tu equipo favorito antes que ver una serie?\n\t","\n\tPrefieres los gameplays sobre los tutoriales de belleza?","\n\tTe afeitas?","\n\tTe gustaria tener una buena barba?","\n\tTienes el cabello regularmente corto?","\n\tHas besado a una mujer en la boca?","\n\tTe has enamorado de una mujer?","\n\tTe gustaria estar super fuerte y con el abdomen marcado?\n\t"};
+	int qdirectory = 0;
+	//srand(time(NULL));
+	//qdirectory = rand() % 9;
+	qdirectory++;
 	printf("%s", mensq[qdirectory]);
 }
 
 void womanquestion()
 {
-	char womensq[][80] = {"11","22","33","44","55","66","77","88","99","10"};
-	int defineq, qdirectory;
-	srand(time(NULL));
-	defineq = rand();
-	qdirectory = rand() % 9;
+	char womensq[][80] = {"\n\tTe casarias con un vestido blanco?\n\t","\n\tSi tuvieses un bebe lo amamantarias?\n\t","\n\tUtlizarias un bolso de mano?\n\t","\n\tHas utilizado tacones en publico?\n\t","\n\tUtilizarias lapiz labial?\n\t","\n\tTe pintarias las unas?\n\t","\n\tHas utilizado un top?\n\t","\n\tHas hablado del chico que te gusta con tu mejor amiga?\n\t","\n\tPrefieres ver tu serie favorita antes que un partido de tu equipo favorito?\n\t","\n\tTe gusta usar maquillaje?\n\t"};
+	int qdirectory = 0;
+	//srand(time(NULL));
+	//qdirectory = rand() % 9;
+	qdirectory++;
 	printf("%s", womensq[qdirectory]);
 }
--- task_progra.c
+++ task_progra.c
@@ -1,51 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "questions.h"
+//#include "questions.h"
 #include <time.h>
 #include <string.h>
 
 int main()
 {
 	int man = 0, woman = 0;
-	char answer[10];
+	int qdirectory = 0;
 
-	printf("\tBienvenido! intentar%c adivinar tu genero basandome\n\ten una serie de preguntas de si o no\n\tEscribe si o no\n\t", 130);
+
+	char answer[10];
+	char mensq[][80] = {"\n\tTe gustan los deportes de contacto?\n\t","\n\tTe gustaria ser como cr7 o messi?\n\t","\n\tprefieres ver un partido de tu equipo favorito antes que ver una serie?\n\t","\n\tPrefieres los gameplays sobre los tutoriales de belleza?\n\t","\n\tTe afeitas?\n\t","\n\tTe gustaria tener una buena barba?\n\t","\n\tTienes el cabello regularmente corto?\n\t","\n\tHas besado a una mujer en la boca?\n","\n\tTe has enamorado de una mujer?\n","\n\tTe gustaria estar super fuerte y con el abdomen marcado?\n\t"};
+	char womensq[][80] = {"\n\tTe casarias con un vestido blanco?\n\t","\n\tSi tuvieses un bebe lo amamantarias?\n\t","\n\tUtlizarias un bolso de mano?\n\t","\n\tHas utilizado tacones en publico?\n\t","\n\tUtilizarias lapiz labial?\n\t","\n\tTe pintarias las unas?\n\t","\n\tHas utilizado un top?\n\t","\n\tHas hablado del chico que te gusta con tu mejor amiga?\n\t","\n\tPrefieres ver tu serie favorita antes que un partido de tu equipo favorito?\n\t","\n\tTe gusta usar maquillaje?\n\t"};
+
+
+	printf("\tBienvenido! intentar%c adivinar tu genero basandome\n\ten una serie de preguntas de si o no\n\t\n\t", 130);
 	
 	while (man < 6 && woman < 6)
 	{
 		srand(time(NULL));
 
 		if((rand() % 2) == 0){
-			menquestion();
+			//menquestion();
+			printf("%s", mensq[qdirectory]);
+
 			scanf("%s", answer);
-			while ( strcmp(answer, "si") == 0 && strcmp(answer, "no") == 0);
+			while (strcmp(answer, "no") != 0 && strcmp(answer, "si") != 0)
 			{
-				printf("\n\tPor favor ingrese \"si\" o \"no\"\n");
-				scanf("%s", answer);
+				printf("\n\tValor incorrecto, por favor escriba si o no\n\t");
+				scanf("%9s", answer);
 			}
-			
-			if(strcmp(answer, "si") == 0){
+			if(strcmp(answer, "si") == 0)
+			{
 				man++;
-			}else
-			{
+			}else{
 				woman++;
-			}
+			}		
+			
 		}else
 		{
-			womanquestion();
+			//womanquestion();
+			printf("%s", womensq[qdirectory]);
 			scanf("%s", answer);
-			while ( strcmp(answer, "si") == 0 && strcmp(answer, "no") == 0);
+			while (strcmp(answer, "no") != 0 && strcmp(answer, "si") != 0)
 			{
-				printf("\n\tPor favor ingrese \"si\" o \"no\"\n");
-				scanf("%s", answer);
+				printf("\n\tValor incorrecto, por favor escriba si o no\n\t");
+				scanf("%9s", answer);
 			}
 			if(strcmp(answer, "si") == 0){
 				woman++;
-			}else
-			{
+			}else{
 				man++;
 			}
 		}
+	qdirectory++;
+	}
+	if(man > woman)
+	{
+		printf("\n\tUsted es un hombre\n");
+	}else if(woman > man)
+	{
+		printf("\n\tusted es mujer\n");
+	}else
+	{
+		printf("\n\tIndefinido, por favor vuelva a intentar\n");
 	}
 
 	return 0;
